7a544b31413816959ccc0d934e8a00303a4ab5f6
Benjamin Kan <Benjamin.Kan@ibm.com>
2022-09-21 23:25:53 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-picture-gallery',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
