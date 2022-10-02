e91af7c0d39f1a39fe646188cc7e5d2d59cfac9b
Benjamin Kan <Benjamin.Kan@ibm.com>
2022-10-02 18:39:36 +0200

    Use modules and align center

--- src/pages/PictureSliderTab.tsx
+++ src/pages/PictureSliderTab.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { IonContent, IonHeader, IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonImg, IonPage, IonTitle, IonToolbar, IonicSlides } from '@ionic/react';
+import { Autoplay, Keyboard, Pagination, Scrollbar, Zoom, EffectCube, EffectCards } from 'swiper';
 import { TakenPicture } from '../hooks/usePictureGallery';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-//import 'swiper/swiper.min.css';
 import 'swiper/css';
+import 'swiper/css/zoom';
+import 'swiper/css/autoplay';
+import 'swiper/css/keyboard';
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-cube';
+import 'swiper/css/effect-cube';
 import '@ionic/react/css/ionic-swiper.css';
 import './PictureSliderTab.css';
 
@@ -13,6 +20,8 @@ interface Props {
 }
 
 const PictureSliderTab: React.FC<Props> = ({ pictures }: Props) => {
+  const effect = pictures.length > 3 ? 'cards' : 'cube';
+
   return (
     <IonPage>
       <IonHeader>
@@ -21,9 +30,17 @@ const PictureSliderTab: React.FC<Props> = ({ pictures }: Props) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <Swiper>
+      <Swiper className='full-height'
+        modules={[Autoplay, Keyboard, Pagination, Scrollbar, Zoom, EffectCube, EffectCards, IonicSlides]}
+        autoplay={{delay: 3000 }}
+        loop
+        keyboard={true}
+        pagination={{clickable: true}}
+        scrollbar={false}
+        zoom={true}
+        effect={effect}>
         {pictures.map(picture => (
-          <SwiperSlide>
+          <SwiperSlide key={picture.fileName}>
             <IonImg src={picture.webviewPath} />
           </SwiperSlide>
         ))}
